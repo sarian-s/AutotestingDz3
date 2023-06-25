3544ec04a80431f8a4af8205b1def92342a053f3
sarian-s <127656330+sarian-s@users.noreply.github.com>
2023-06-25 04:18:04 +0300

    Update AppOrderTest.java

--- src/test/java/ru/netology/AppOrderTest.java
+++ src/test/java/ru/netology/AppOrderTest.java
@@ -11,7 +11,7 @@ public class AppOrderTest {
 
     @Test
     public void Test1() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
 
         SelenideElement form = $("form");
         form.$("input[name=name]").setValue("Иванов Иван");
@@ -25,7 +25,7 @@ public class AppOrderTest {
 
     @Test
     public void Test2() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
 
         SelenideElement form = $("form");
         form.$("input[name=name]").setValue("Иванов Иdfy");
@@ -39,7 +39,7 @@ public class AppOrderTest {
 
     @Test
     public void Test3() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
 
         SelenideElement form = $("form");
         form.$("input[name=name]").setValue("Иванов иван");
@@ -53,7 +53,7 @@ public class AppOrderTest {
 
     @Test
     public void Test4() {
-        open("http://0.0.0.0:9999");
+        open("http://localhost:9999");
 
         SelenideElement form = $("form");
         form.$("input[name=name]").setValue("Иванов иван");
