ab82b63abdeec6f842e60758b898d7b2459ef0e0
sergey s <som-ana@yandex.ru>
2023-06-25 13:35:36 +0300

    Changed appveyor config

--- src/test/java/ru/netology/AppOrderTest.java
+++ src/test/java/ru/netology/AppOrderTest.java
@@ -1,26 +1,15 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class AppOrderTest {
-    WebDriver driver;
-    @BeforeEach
-    void setup() {
-        driver = WebDriverManager.chromedriver().create();
-    }
-
-
     @Test
     public void Test1() {
-
         open("http://localhost:9999");
 
         SelenideElement form = $("form");
@@ -29,8 +18,6 @@ public class AppOrderTest {
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
         $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-
-
     }
 
     @Test
@@ -43,8 +30,6 @@ public class AppOrderTest {
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
         form.$("[data-test-id=name]").$(".input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-
-
     }
 
     @Test
@@ -57,8 +42,6 @@ public class AppOrderTest {
         form.$("[data-test-id=agreement]").click();
         form.$("button").click();
         form.$("[data-test-id=phone]").$(".input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
-
-
     }
 
     @Test
@@ -70,9 +53,5 @@ public class AppOrderTest {
         form.$("input[name=phone]").setValue("+12345678900");
         form.$("button").click();
         form.$("[data-test-id=agreement]").shouldHave(Condition.cssClass("input_invalid"));
-
-
     }
-
-
 }
